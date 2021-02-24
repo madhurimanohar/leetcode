3a18953dfea8ea129d6260513d4f8a07e3e6b6af
Madhuri Manohar <28357066+madhurimanohar@users.noreply.github.com>
2021-02-23 22:01:50 -0800

    Check If Two String Arrays Are Equivalent

---
+++ 1662.CheckIfTwoStringArraysAreEquivalent/CheckIfTwoStringArraysAreEquivalent.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String a = "";
+        String b = "" ;
+        
+        for(int i = 0; i < word1.length; i++) {
+            a += word1[i];
+        }
+        
+        for(int j = 0; j < word2.length; j++) {
+            b += word2[j];
+        }
+        
+        if(a.compareTo(b) == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
